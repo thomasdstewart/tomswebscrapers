fff2f1a14d4fe6f037c40375d4cedc137e52c931
Thomas Stewart <thomas@stewarts.org.uk>
2018-07-01 19:19:54 +0100

    barclaysscrape.py: autopep8 and config file fixes

--- barclaysscrape.py
+++ barclaysscrape.py
@@ -29,47 +29,58 @@ import sys
 import csv
 
 from pprint import pprint as pp
-#import ipdb
-#from IPython import embed; embed()
+#import ipdb; from IPython import embed; embed()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--debug", \
-    help="log to stdout and save screenshots and html", action="store_true", \
-    default=False)
-parser.add_argument("-p", "--proxy", \
-    help="http proxy to use (eg localhost:8888)")
+parser.add_argument("-d", "--debug",
+                    help="log to stdout and save screenshots and html", action="store_true",
+                    default=False)
+parser.add_argument("-p", "--proxy",
+                    help="http proxy to use (eg localhost:8888)")
 args = parser.parse_args()
 
 if(args.debug):
-    logging.basicConfig(level=logging.INFO, \
-        format="%(levelname)s %(asctime)s %(message)s")
+    logging.basicConfig(level=logging.INFO,
+                        format="%(levelname)s %(asctime)s %(message)s")
 else:
-    logging.basicConfig(filename="barclaysscrape.log", \
-        level=logging.INFO,format='%(levelname)s %(asctime)s %(message)s')
+    logging.basicConfig(filename="barclaysscrape.log",
+                        level=logging.INFO, format='%(levelname)s %(asctime)s %(message)s')
 
 logging.info("reading config")
+configfile = 'barclaysscrape.ini'
 config = configparser.ConfigParser()
-config.read("barclaysscrape.ini")
+config.read(configfile)
 
-surname = config.get("barclaysscrape", "surname")
-membershipnum = config.get("barclaysscrape", "membershipnum")
-passcode = config.get("barclaysscrape", "passcode")
-password = config.get("barclaysscrape", "password")
+if(len(config.sections()) == 0):
+    logging.info("could not read %s" % configfile)
+    sys.exit()
+
+if('barclaysscrape' not in config):
+    logging.info("could not read barclaysscrape section in %s" % configfile)
+    sys.exit()
+
+section = config['barclaysscrape']
+if('surname' not in section or 'membershipnum' not in section or 'passcode' not in section or 'password' not in section):
+    logging.info(
+        "could not find setting in barclaysscrape section in %s" % configfile)
+    sys.exit()
+
+surname = config['barclaysscrape']['surname']
+membershipnum = config['barclaysscrape']['membershipnum']
+passcode = config['barclaysscrape']['passcode']
+password = config['barclaysscrape']['password']
 
 logging.info("setting up selenium")
-chrome_options = selenium.webdriver.chrome.options.Options()  
-chrome_options.add_argument("--headless")  
+chrome_options = selenium.webdriver.chrome.options.Options()
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1300,1600")
-#default UA is HeadlessChrome rather than Chrome
-chrome_options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) " + \
-    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36")
-
-#if ('http_proxy' in os.environ.keys()):
-#    proxy = os.environ['http_proxy']
-#    logging.info("using proxy %s" % proxy)
-#    chrome_options.add_argument("--proxy-server=%s" % proxy)
+# default UA is HeadlessChrome rather than Chrome
+chrome_options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) " +
+                            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36")
 
 if(args.proxy):
+    # if('http_proxy' in os.environ.keys()):
+    #    args.proxy = os.environ['http_proxy']
     logging.info("using proxy: %s" % args.proxy)
     chrome_options.add_argument("--proxy-server=%s" % args.proxy)
 else:
@@ -79,17 +90,19 @@ logging.info("starting selenium chrome headless")
 driver = selenium.webdriver.Chrome(chrome_options=chrome_options)
 driver.implicitly_wait(5)
 
-n=1
-def shotnhtml(d,n):
+n = 1
+
+def shotnhtml(d, n):
     if(not args.debug):
         return n
-    logging.info("saving barclaysscrape-%02d.png and barclaysscrape-%02d.html" \
-        % (n,n))
+    logging.info("saving barclaysscrape-%02d.png and barclaysscrape-%02d.html"
+                 % (n, n))
     d.save_screenshot("barclaysscrape-%02d.png" % n)
     file = open("barclaysscrape-%02d.html" % n, "w")
     file.write(d.page_source)
     file.close()
     return n + 1
+
 
 loginurl = "https://bank.barclays.co.uk/olb/auth/LoginLink.action"
 logging.info("getting page:%s", loginurl)
@@ -121,9 +134,9 @@ n = shotnhtml(driver, n)
 logging.info("filling passcode and password")
 driver.find_element_by_id("passcode0").send_keys(passcode)
 
-first  = driver.find_element_by_xpath(\
+first = driver.find_element_by_xpath(
     "//label[@id='label-memorableCharacters']/strong[1]").text
-second = driver.find_element_by_xpath(\
+second = driver.find_element_by_xpath(
     "//label[@id='label-memorableCharacters']/strong[2]").text
 first = int(first[0:1])
 second = int(second[0:1])
@@ -131,11 +144,10 @@ second = int(second[0:1])
 first = str(password[first-1:first])
 second = str(password[second-1:second])
 
-#driver.find_elements_by_xpath("//*[@id='idForScrollFeature']/div[1]")[0].send_keys(str(first))
-#driver.find_elements_by_xpath("//*[@id='idForScrollFeature']/div[1]")[0].send_keys(str(second))
-
-driver.find_element_by_xpath("//div[@name='firstMemorableCharacter']/div").send_keys(first)
-driver.find_element_by_xpath("//div[@name='secondMemorableCharacter']/div").send_keys(second)
+driver.find_element_by_xpath(
+    "//div[@name='firstMemorableCharacter']/div").send_keys(first)
+driver.find_element_by_xpath(
+    "//div[@name='secondMemorableCharacter']/div").send_keys(second)
 driver.find_element_by_xpath("//button").click()
 time.sleep(5)
 n = shotnhtml(driver, n)
@@ -146,7 +158,7 @@ c = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
 logging.info("getting account list")
 accounts = {}
-for account in driver.find_elements_by_xpath( \
+for account in driver.find_elements_by_xpath(
         "//*[@class='account-detail-wrapper summary']"):
     a = account.get_attribute("aria-label")
     pa = a.replace(' ', '')
@@ -157,8 +169,8 @@ logging.info("found accounts %s" % ",".join(accounts.keys()))
 
 for account in accounts:
     logging.info("getting account %s" % account)
-    driver.find_element_by_xpath( \
-        "//*[@aria-label='%s']/..//*[@id='showStatements']" \
+    driver.find_element_by_xpath(
+        "//*[@aria-label='%s']/..//*[@id='showStatements']"
         % accounts[account]).click()
     time.sleep(5)
     n = shotnhtml(driver, n)
